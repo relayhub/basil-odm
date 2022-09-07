409c2bc6f2177d8748c2086a772f0dd9bc516c8a
Kubota Mitsunori <anatoo.jp@gmail.com>
2022-09-07 22:50:03 +0900

    feat: add code generator for enums()

---
+++ src/generator/_tests/generateEnums.test.ts
@@ -0,0 +1,37 @@
+import { enums } from '../../schema/enums';
+import { generateEnumsCode } from '../codeGenerator';
+import { CollectionSchema } from '../../schema/CollectionSchema';
+import { objectId } from '../../schema/objectId';
+
+describe('generateEnums()', () => {
+  test('normal', () => {
+    const collection = new CollectionSchema({
+      collectionName: 'users',
+      entityName: 'User',
+      fields: {
+        _id: objectId,
+        type: enums({
+          name: 'UserType',
+          values: ['member', 'admin'],
+        }),
+      },
+    });
+
+    expect(generateEnumsCode([collection])).toMatchSnapshot();
+  });
+
+  test('without name', () => {
+    const collection = new CollectionSchema({
+      collectionName: 'users',
+      entityName: 'User',
+      fields: {
+        _id: objectId,
+        type: enums({
+          values: ['member', 'admin'],
+        }),
+      },
+    });
+
+    expect(generateEnumsCode([collection])).toMatchSnapshot();
+  });
+});
