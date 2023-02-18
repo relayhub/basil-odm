a20a8a89532b264f8b29c4227f0cf90f768a24e2
Kubota Mitsunori <anatoo.jp@gmail.com>
2023-02-18 11:58:05 +0900

    fix: fix the behavior when duplicated enum name

--- src/generator/generateEnums.test.ts
+++ src/generator/generateEnums.test.ts
@@ -4,7 +4,7 @@ import { CollectionSchema } from '../schema/CollectionSchema';
 import { objectId } from '../schema/objectId';
 
 describe('generateEnums()', () => {
-  test('normal', () => {
+  it('should works normally', () => {
     const collection = new CollectionSchema({
       collectionName: 'users',
       entityName: 'User',
@@ -20,7 +20,7 @@ describe('generateEnums()', () => {
     expect(generateEnumsCode([collection])).toMatchSnapshot();
   });
 
-  test('without name', () => {
+  it('should work without name', () => {
     const collection = new CollectionSchema({
       collectionName: 'users',
       entityName: 'User',
@@ -33,5 +33,43 @@ describe('generateEnums()', () => {
     });
 
     expect(generateEnumsCode([collection])).toMatchSnapshot();
+  });
+
+  it('should work with duplicated name', () => {
+    const userType = enums({
+      name: 'UserType',
+      values: ['member', 'admin'],
+    });
+    const collection = new CollectionSchema({
+      collectionName: 'users',
+      entityName: 'User',
+      fields: {
+        _id: objectId,
+        type: userType,
+        prevType: userType,
+      },
+    });
+
+    expect(generateEnumsCode([collection])).toMatchSnapshot();
+  });
+
+  it('should throw error', () => {
+    const collection = new CollectionSchema({
+      collectionName: 'users',
+      entityName: 'User',
+      fields: {
+        _id: objectId,
+        type: enums({
+          name: 'UserType',
+          values: ['member', 'admin'],
+        }),
+        prevType: enums({
+          name: 'UserType',
+          values: ['guest', 'member', 'admin'],
+        }),
+      },
+    });
+
+    expect(() => generateEnumsCode([collection])).toThrowError();
   });
 });
