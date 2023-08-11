29e7ee7c5c83d5948e834c0d9a92a8513fcf9b60
Kubota Mitsunori <anatoo.jp@gmail.com>
2023-08-11 14:57:45 +0900

    refactor

--- src/schema/collection.ts
+++ src/schema/collection.ts
@@ -1,5 +1,5 @@
 import { CollectionDef, Index } from '../types';
-import { createFieldsSchema, FieldsSchema } from './FieldsSchema';
+import { createFieldsSchema } from './FieldsSchema';
 import { ObjectSchemaSource } from './types';
 
 interface Props {
@@ -9,36 +9,11 @@ interface Props {
   entityName?: string;
 }
 
-class CollectionSchema implements CollectionDef {
-  _collectionName: string;
-  _fields: FieldsSchema;
-  _indexes: Index[];
-  _entityName: string | null = null;
-
-  constructor(props: Props) {
-    const { collectionName, fields = {}, indexes = [], entityName = null } = props;
-
-    this._collectionName = collectionName;
-    this._fields = createFieldsSchema(fields);
-    this._indexes = indexes;
-    this._entityName = entityName;
-  }
-
-  get collectionName(): string {
-    return this._collectionName;
-  }
-
-  get entityName(): string | null {
-    return this._entityName;
-  }
-
-  get schema(): FieldsSchema {
-    return this._fields;
-  }
-
-  get indexes(): Index[] {
-    return this._indexes;
-  }
+export function collection(props: Props): CollectionDef {
+  return {
+    collectionName: props.collectionName,
+    entityName: props.entityName || null,
+    schema: createFieldsSchema(props.fields || {}),
+    indexes: props.indexes || [],
+  };
 }
-
-export const collection = (props: Props): CollectionSchema => new CollectionSchema(props);
