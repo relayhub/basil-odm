6bf78bc3131b921b2794ea2b5030970161489704
Kubota Mitsunori <anatoo.jp@gmail.com>
2023-11-09 22:02:22 +0900

    test: use CLI to generate code for e2e testcases

--- e2e/01/generate.ts
+++
@@ -1,9 +0,0 @@
-import { generateCode } from '../../src';
-import { join } from 'path';
-import * as schema from './schema';
-
-generateCode({
-  schema,
-  outputFile: join(__dirname, 'basil-gen.ts'),
-  importSource: '../../src',
-});
--- e2e/01/prepare-db.ts
+++
@@ -1,9 +0,0 @@
-import { prepareDb, disconnect } from '../../src';
-import * as schema from './schema';
-
-const prepare = async () => {
-  await prepareDb(schema);
-  await disconnect();
-};
-
-prepare().catch(console.log);
