2daca8faaa91b4f9f475b691a463afbec4bbc84c
Kubota Mitsunori <anatoo.jp@gmail.com>
2023-11-10 07:00:12 +0900

    fix: fix a lint error

--- test/globalTeardown.ts
+++ test/globalTeardown.ts
@@ -1,6 +1,6 @@
 import { MongoMemoryReplSet } from 'mongodb-memory-server';
 
 export = async function globalTeardown() {
-  const instance = (global as any).__MONGOINSTANCE as MongoMemoryReplSet;
+  const instance = (global as unknown as { __MONGOINSTANCE: MongoMemoryReplSet }).__MONGOINSTANCE;
   await instance.stop();
 };
